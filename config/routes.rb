2056fb78bcf9a2ad73bc8b239d69126493ce6989
Zach T <ztrokey@gmail.com>
2021-05-11 18:27:48 -0600

    Add show function for dealerships

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/manufacturers', to: 'manufacturers#index'
   get '/dealerships', to: 'dealerships#index'
+  get '/dealerships/:id', to: 'dealerships#show'
 end
+
