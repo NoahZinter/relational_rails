9e74f76fe656e22c4b0c09d0d0f857325bf8e6b1
NoahZinter <noah.zinter@gmail.com>
2021-05-12 11:30:16 -0600

    Create vehicles show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
+
   get '/manufacturers', to: 'manufacturers#index'
   get '/manufacturers/:id', to: 'manufacturers#show'
+
+  get '/vehicles', to: 'vehicles#index'
+  get '/vehicles/:id', to: 'vehicles#show'
+
   get '/dealerships', to: 'dealerships#index'
   get '/dealerships/:id', to: 'dealerships#show'
+
   get '/employees', to: 'employees#index'
-  get '/vehicles', to: 'vehicles#index'
   get '/employees/:id', to: 'employees#show'
 end
