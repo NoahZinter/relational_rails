75fc22507182e57176c6c0556eccc11cc2e275b1
NoahZinter <noah.zinter@gmail.com>
2021-05-13 14:47:01 -0600

    Test existence of created at on page

--- spec/features/manufacturers/manufacturers_index_spec.rb
+++ spec/features/manufacturers/manufacturers_index_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe "Manufacturer Index" do
    expect(page).to have_content(chevy.name)
    expect(page).to have_content(zonda.name)
   end
+
+  it 'shows the created at time for each manufacturer' do
+   chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
+   zonda = Manufacturer.create!(name:"Zonda", production_capacity:80, is_open:true)
+
+   visit"/manufacturers"
+
+   expect(page).to have_content(chevy.created_at)
+   expect(page).to have_content(zonda.created_at)
+  end
 end
