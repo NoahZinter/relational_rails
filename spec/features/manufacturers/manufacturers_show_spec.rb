4e9781b9d7de42c9c46c23e2ca4e4cde11e67627
NoahZinter <noah.zinter@gmail.com>
2021-05-13 13:10:37 -0600

    Test manufacturer show attributes

--- spec/features/manufacturers/manufacturers_show_spec.rb
+++ spec/features/manufacturers/manufacturers_show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Manufacturer Index" do
-  it 'shows an index of all manufacturers' do
+RSpec.describe "Manufacturer Show" do
+  it 'shows only the selected manufacturer' do
    chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
    zonda = Manufacturer.create!(name:"Zonda", production_capacity:80, is_open:true)
 
@@ -9,5 +9,15 @@ RSpec.describe "Manufacturer Index" do
 
    expect(page).to have_content(chevy.name)
    expect(page).not_to have_content(zonda.name)
+  end
+
+  it 'shows selected manufacturers attributes' do
+   chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
+
+   visit"/manufacturers/#{chevy.id}"
+
+   expect(page).to have_content(chevy.name)
+   expect(page).to have_content(chevy.production_capacity)
+   expect(page).to have_content(chevy.is_open)
   end
 end
---
+++ spec/features/vehicles/vehicle_index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Vehicle Index" do
+  it 'shows an index of all manufacturers' do
+   chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
+   zonda = Manufacturer.create!(name:"Zonda", production_capacity:80, is_open:true)
+
+   visit"/manufacturers"
+
+   expect(page).to have_content(chevy.name)
+   expect(page).to have_content(zonda.name)
+  end
+end
