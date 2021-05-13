ca793b9dc9edfd5fe6d3240ac4a0194d7eaca6e4
NoahZinter <noah.zinter@gmail.com>
2021-05-13 13:17:17 -0600

    Test vehicle index

--- spec/features/vehicles/vehicle_index_spec.rb
+++ spec/features/vehicles/vehicle_index_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Vehicle Index" do
-  it 'shows an index of all manufacturers' do
-   chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
-   zonda = Manufacturer.create!(name:"Zonda", production_capacity:80, is_open:true)
+  it 'shows an index of all vehicles' do
+    honda = Manufacturer.create!(name:"Honda", production_capacity: 28, is_open:true)
+    civic = honda.vehicles.create!(name:"Civic", year:2000, price:2500, sold:false)
+    crv = honda.vehicles.create!(name:"CRV", year:2005, price:4500, sold:true)
 
-   visit"/manufacturers"
+   visit"/vehicles"
 
-   expect(page).to have_content(chevy.name)
-   expect(page).to have_content(zonda.name)
+   expect(page).to have_content(civic.name)
+   expect(page).to have_content(civic.year)
+   expect(page).to have_content(civic.price)
+   expect(page).to have_content(civic.sold)
+   expect(page).to have_content(crv.name)
+   expect(page).to have_content(crv.year)
+   expect(page).to have_content(crv.price)
+   expect(page).to have_content(crv.sold)
   end
 end
