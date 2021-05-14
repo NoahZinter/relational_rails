769ab2b3c57d67ab7052f39a3f3591a4d6661d31
Zach T <ztrokey@gmail.com>
2021-05-14 08:10:20 -0600

    Add tests and methods for create dealership

--- app/controllers/dealerships_controller.rb
+++ app/controllers/dealerships_controller.rb
@@ -7,4 +7,17 @@ class DealershipsController < ApplicationController
   def show
     @dealership = Dealership.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    dealership = Dealership.create(name: params[:name],
+                                   is_open: params[:is_open],
+                                   max_car_capacity: params[:max_car_capacity],
+                                   is_full: params[:is_full])
+    redirect_to "/dealerships"
+    # require 'pry'; binding.pry
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,12 @@ Rails.application.routes.draw do
   get '/vehicles/:id', to: 'vehicles#show'
 
   get '/dealerships', to: 'dealerships#index'
+  get '/dealerships/new', to: 'dealerships#new'
   get '/dealerships/:id', to: 'dealerships#show'
+  get '/dealerships/:id/employees', to: 'dealerships_employees#index'
+  post '/dealerships', to: 'dealerships#create'
 
   get '/employees', to: 'employees#index'
   get '/employees/:id', to: 'employees#show'
-  get '/dealerships/:id/employees', to: 'dealerships_employees#index'
+
 end
