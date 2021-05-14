397cf82d4e26cf45cf52b4bb24efdd94f48d6a75
NoahZinter <noah.zinter@gmail.com>
2021-05-13 18:35:32 -0600

    Refactor for hound comments

--- spec/features/manufacturers/manufacturers_show_spec.rb
+++ spec/features/manufacturers/manufacturers_show_spec.rb
@@ -2,42 +2,42 @@ require 'rails_helper'
 
 RSpec.describe "Manufacturer Show" do
   it 'shows only the selected manufacturer' do
-   chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
-   zonda = Manufacturer.create!(name:"Zonda", production_capacity:80, is_open:true)
+    chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
+    zonda = Manufacturer.create!(name:"Zonda", production_capacity:80, is_open:true)
 
-   visit"/manufacturers/#{chevy.id}"
+    visit"/manufacturers/#{chevy.id}"
 
-   expect(page).to have_content(chevy.name)
-   expect(page).not_to have_content(zonda.name)
+    expect(page).to have_content(chevy.name)
+    expect(page).not_to have_content(zonda.name)
   end
 
   it 'shows selected manufacturers attributes' do
-   chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
+    chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
 
-   visit"/manufacturers/#{chevy.id}"
+    visit"/manufacturers/#{chevy.id}"
 
-   expect(page).to have_content(chevy.name)
-   expect(page).to have_content(chevy.production_capacity)
-   expect(page).to have_content(chevy.is_open)
+    expect(page).to have_content(chevy.name)
+    expect(page).to have_content(chevy.production_capacity)
+    expect(page).to have_content(chevy.is_open)
   end
 
   it 'counts vehicles of selected manufacturer' do
-   chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
-   cruz = chevy.vehicles.create!(name:"Cruz", year:2000, price:2500, sold:false)
-   silverado = chevy.vehicles.create!(name:"Silverado", year:2005, price:4500, sold:true)
+    chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
+    cruz = chevy.vehicles.create!(name:"Cruz", year:2000, price:2500, sold:false)
+    silverado = chevy.vehicles.create!(name:"Silverado", year:2005, price:4500, sold:true)
 
-   visit"/manufacturers/#{chevy.id}"
+    visit"/manufacturers/#{chevy.id}"
 
-   expect(page).to have_content(chevy.vehicle_count)
+    expect(page).to have_content(chevy.vehicle_count)
   end
 
   it 'includes a link to a list of all vehicles for manufacturer' do
-   chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
-   cruz = chevy.vehicles.create!(name:"Cruz", year:2000, price:2500, sold:false)
-   silverado = chevy.vehicles.create!(name:"Silverado", year:2005, price:4500, sold:true)
+    chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
+    cruz = chevy.vehicles.create!(name:"Cruz", year:2000, price:2500, sold:false)
+    silverado = chevy.vehicles.create!(name:"Silverado", year:2005, price:4500, sold:true)
 
-   visit"/manufacturers/#{chevy.id}"
+    visit"/manufacturers/#{chevy.id}"
 
-   expect(page).to have_link("All Chevy Vehicles:", :href => "/manufacturers/#{chevy.id}/vehicles")
+    expect(page).to have_link("All Chevy Vehicles:", :href => "/manufacturers/#{chevy.id}/vehicles")
   end
 end
