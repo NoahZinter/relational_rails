3122d619353fec5c75942db504bd6eee215c9702
NoahZinter <noah.zinter@gmail.com>
2021-05-15 17:36:29 -0600

    Test vehicle edit button

--- spec/features/vehicles/vehicle_show_spec.rb
+++ spec/features/vehicles/vehicle_show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Vehicle Index" do
-  it 'shows an index of all vehicles' do
+  it 'displays only the vehicle with the given id' do
     honda = Manufacturer.create!(name:"Honda", production_capacity: 28, is_open:true)
     civic = honda.vehicles.create!(name:"Civic", year:2000, price:2500, sold:false)
     crv = honda.vehicles.create!(name:"CRV", year:2005, price:4500, sold:true)
@@ -16,5 +16,14 @@ RSpec.describe "Vehicle Index" do
    expect(page).not_to have_content(crv.year)
    expect(page).not_to have_content(crv.price)
    expect(page).not_to have_content(crv.sold)
+  end
+
+  it 'displays a button to edit the vehicle' do
+    honda = Manufacturer.create!(name:"Honda", production_capacity: 28, is_open:true)
+    civic = honda.vehicles.create!(name:"Civic", year:2000, price:2500, sold:false)
+
+    visit"/vehicles/#{civic.id}"
+
+    expect(page).to have_button('Edit This Civic')
   end
 end
