2aac45662e61adc495c29e8c12d1c1914e99d598
NoahZinter <noah.zinter@gmail.com>
2021-05-15 17:17:30 -0600

    Refactor manufacturers_new_spec for select box

--- spec/features/manufacturers/manufacturers_new_spec.rb
+++ spec/features/manufacturers/manufacturers_new_spec.rb
@@ -1,12 +1,25 @@
 require "rails_helper"
 
 RSpec.describe 'Manufacturer New' do
-  it 'contains a form to create a new manufacturer' do
-    visit"/manufacturers/new"
+  it 'contains a form for creating new manufacturer' do
+    visit 'manufacturers/new'
 
-    fill_in('Name', with:'Peugot')
+    expect(page).to have_field('Name')
+    expect(page).to have_field('Production capacity')
+      
+      within("select#sold") do
+        %w(true false).each do |option|
+          expect(find("option[value=#{option}]").text).to eq(option)
+        end
+      end
+  end
+
+  it 'creates new manufacturer and navigates to index' do
+    visit 'manufacturers/new'
+
+    fill_in('Name', with: 'Peugot')
     fill_in('Production capacity', with: 200)
-    check('Is open')
+    select('false')
     click_button('Create Manufacturer')
 
     expect(current_path).to eq('/manufacturers')
