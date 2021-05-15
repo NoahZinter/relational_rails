2a62677382d289f3ab4d48a94b1c964bf8b7c140
Zach T <ztrokey@gmail.com>
2021-05-15 09:42:17 -0600

    Complete new dealership/empoloyee tests and methods

--- app/controllers/dealerships_employees_controller.rb
+++ app/controllers/dealerships_employees_controller.rb
@@ -9,6 +9,16 @@ class DealershipsEmployeesController < ApplicationController
   end
 
   def create
-    # require 'pry'; binding.pry
+    employee = Dealership.find(params[:id]).employees.create(employee_params)
+    redirect_to "/dealerships/#{Dealership.find(params[:id]).id}/employees"
+  end
+
+  private
+  def employee_params
+    params.permit(
+                  :name,
+                  :on_vacation,
+                  :cars_sold
+                 )
   end
 end
--- spec/features/dealerships/employees/new_spec.rb
+++ spec/features/dealerships/employees/new_spec.rb
@@ -1,16 +1,3 @@
-# User Story 13, Parent Child Creation (x2)
-
-# As a visitor
-# When I visit a Parent Childs Index page
-# Then I see a link to add a new adoptable child for that parent "Create Child"
-# When I click the link
-# I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child
-# When I fill in the form with the child's attributes:
-# And I click the button "Create Child"
-# Then a `POST` request is sent to '/parents/:parent_id/child_table_name',
-# a new child object/row is created for that parent,
-# and I am redirected to the Parent Childs Index page where I can see the new child listed
-
 require 'rails_helper'
 
 RSpec.describe 'New Dealership Child' do
@@ -19,7 +6,7 @@ RSpec.describe 'New Dealership Child' do
       teds = Dealership.create!(name:"Ted's Auto", is_open: true, max_car_capacity: 540, is_full: false)
 
       visit "/dealerships/#{teds.id}/employees"
-      # save_and_open_page
+
       click_link "Create Employee"
 
       expect(current_path).to eq("/dealerships/#{teds.id}/employees/new")
