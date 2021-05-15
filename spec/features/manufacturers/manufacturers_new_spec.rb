8932ec079c7c53b28194d9a95abdddd43236a199
NoahZinter <noah.zinter@gmail.com>
2021-05-15 09:18:20 -0600

    Create manufacturer new form

--- spec/features/manufacturers/manufacturers_new_spec.rb
+++ spec/features/manufacturers/manufacturers_new_spec.rb
@@ -1,9 +1,15 @@
 require "rails_helper"
 
 RSpec.describe 'Manufacturer New' do
-  xit 'contains a form to create a new manufacturer' do
+  it 'contains a form to create a new manufacturer' do
     visit"/manufacturers/new"
 
-    expect(page).to
+    fill_in('Name', with:'Peugot')
+    fill_in('Production Capacity', with: 200)
+    fill_in('Is Open', with: true)
+    click_button('Create Manufacturer')
+
+    expect(current_path).to eq('/manufacturers/index')
+    expect(page).to have_content('Peugot')
   end
 end
