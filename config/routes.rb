6f48bb3b4b753f12e2a3e9b9a49f4f293355b165
Zach T <ztrokey@gmail.com>
2021-05-15 15:00:13 -0600

    Clean up commented code and line spacing

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,13 @@ Rails.application.routes.draw do
   get '/dealerships', to: 'dealerships#index'
   get '/dealerships/new', to: 'dealerships#new'
   get '/dealerships/:id', to: 'dealerships#show'
-  get '/dealerships/:id/employees', to: 'dealerships_employees#index'
-  post '/dealerships/:id/employees', to: 'dealerships_employees#create'
-  get '/dealerships/:id/employees/new', to: 'dealerships_employees#new'
   get '/dealerships/:id/edit', to: 'dealerships#edit'
   post '/dealerships', to: 'dealerships#create'
   patch '/dealerships/:id', to: 'dealerships#update'
+
+  get '/dealerships/:id/employees', to: 'dealerships_employees#index'
+  post '/dealerships/:id/employees', to: 'dealerships_employees#create'
+  get '/dealerships/:id/employees/new', to: 'dealerships_employees#new'
 
   get '/employees', to: 'employees#index'
   get '/employees/:id', to: 'employees#show'
