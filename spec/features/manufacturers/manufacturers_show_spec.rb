ea9c779a1c68ef6e18388add4ffefa09e3fada9d
NoahZinter <noah.zinter@gmail.com>
2021-05-15 10:51:46 -0600

    Refactor manufacturer show spec

--- spec/features/manufacturers/manufacturers_show_spec.rb
+++ spec/features/manufacturers/manufacturers_show_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Manufacturer Show" do
   it 'shows only the selected manufacturer' do
-    chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
-    zonda = Manufacturer.create!(name:"Zonda", production_capacity:80, is_open:true)
+    chevy = Manufacturer.create!(name: 'Chevy', production_capacity:40, is_open:true)
+    zonda = Manufacturer.create!(name:'Zonda', production_capacity:80, is_open:true)
 
     visit"/manufacturers/#{chevy.id}"
 
@@ -12,7 +12,7 @@ RSpec.describe "Manufacturer Show" do
   end
 
   it 'shows selected manufacturers attributes' do
-    chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
+    chevy = Manufacturer.create!(name: 'Chevy', production_capacity:40, is_open:true)
 
     visit"/manufacturers/#{chevy.id}"
 
@@ -22,9 +22,9 @@ RSpec.describe "Manufacturer Show" do
   end
 
   it 'counts vehicles of selected manufacturer' do
-    chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
-    cruz = chevy.vehicles.create!(name:"Cruz", year:2000, price:2500, sold:false)
-    silverado = chevy.vehicles.create!(name:"Silverado", year:2005, price:4500, sold:true)
+    chevy = Manufacturer.create!(name: 'Chevy', production_capacity:40, is_open:true)
+    chevy.vehicles.create!(name:'Cruz', year:2000, price:2500, sold:false)
+    chevy.vehicles.create!(name:"Silverado", year:2005, price:4500, sold:true)
 
     visit "/manufacturers/#{chevy.id}"
 
@@ -32,12 +32,14 @@ RSpec.describe "Manufacturer Show" do
   end
 
   it 'includes a link to a list of all vehicles for manufacturer' do
-    chevy = Manufacturer.create!(name:"Chevy", production_capacity:40, is_open:true)
-    cruz = chevy.vehicles.create!(name:"Cruz", year:2000, price:2500, sold:false)
-    silverado = chevy.vehicles.create!(name:"Silverado", year:2005, price:4500, sold:true)
+    chevy = Manufacturer.create!(name: 'Chevy', production_capacity: 40, is_open: true)
+    chevy.vehicles.create!(name:'Cruz', year:2000, price:2500, sold: false)
+    chevy.vehicles.create!(name: 'Silverado', year: 2005, price: 4500, sold: true)
 
     visit"/manufacturers/#{chevy.id}"
 
     expect(page).to have_link("All Chevy Vehicles:", :href => "/manufacturers/#{chevy.id}/vehicles")
   end
+
+
 end
