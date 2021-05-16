1a1ebb394cffdde92c557332737e103752854833
NoahZinter <noah.zinter@gmail.com>
2021-05-15 18:03:44 -0600

    Test Vehicle Update

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -10,4 +10,14 @@ class VehiclesController < ApplicationController
   def edit
     @vehicle = Vehicle.find(params[:id])
   end
+
+  def vehicle_params
+    params.permit(:name, :year, :price, :sold)
+  end
+
+  def update
+    @vehicle = Vehicle.find(params[:id])
+    @vehicle.update(vehicle_params)
+    redirect_to "/vehicles/#{@vehicle.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get '/vehicles', to: 'vehicles#index'
   get '/vehicles/:id', to: 'vehicles#show'
   get '/vehicles/:id/edit', to: 'vehicles#edit'
+  patch '/vehicles/:id', to: 'vehicles#update'
 
   get '/dealerships', to: 'dealerships#index'
   get '/dealerships/new', to: 'dealerships#new'
