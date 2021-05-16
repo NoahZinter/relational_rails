84fc13e95cfd5a2684c2e2f7cdf5194aeb526789
NoahZinter <noah.zinter@gmail.com>
2021-05-16 12:32:09 -0600

    Test existence of alphabetize button

--- spec/features/manufacturers/vehicles/index_spec.rb
+++ spec/features/manufacturers/vehicles/index_spec.rb
@@ -19,4 +19,26 @@ RSpec.describe 'Manufacturer Vehicles Index' do
 
     expect(page).to have_button("Create a New #{manufacturer.name} Vehicle:")
   end
+
+  it 'contains a button to alphabetize vehicles' do
+    manufacturer = Manufacturer.create!(name: 'Zonda', production_capacity:40, is_open:true)
+
+    visit "/manufacturers/#{manufacturer.id}/vehicles"
+
+    expect(page).to have_button("Alphabetize Zonda Vehicles")
+  end
+
+  xit 'alphabetizes vehicles' do
+    honda = Manufacturer.create!(name:"Honda", production_capacity: 28, is_open: true)
+    civic = honda.vehicles.create!(name:"Civic", year:2000, price:2500, sold: false)
+    del_sol = honda.vehicles.create!(name:"Del Sol", year:2005, price:4500, sold: false)
+    crv = honda.vehicles.create!(name:"CRV", year:2005, price:4500, sold: false)
+    accord = honda.vehicles.create!(name:"Accord", year:2000, price:2500, sold: false)
+
+    visit "/manufacturers/#{honda.id}/vehicles"
+
+    expect(del_sol.name).to appear_before(accord.name)
+
+    expect(accord.name).to appear_before(del_sol.name)
+  end
 end
