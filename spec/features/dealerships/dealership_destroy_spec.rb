1dd529525c7fe0fbbc663829bda430dddced5c4c
Zach T <ztrokey@gmail.com>
2021-05-17 08:11:30 -0600

    Correct location of delete button and tests per US19

--- spec/features/dealerships/dealership_destroy_spec.rb
+++ spec/features/dealerships/dealership_destroy_spec.rb
@@ -1,20 +1,10 @@
-# User Story 19, Parent Delete (x2)
-
-# As a visitor
-# When I visit a parent show page
-# Then I see a link to delete the parent
-# When I click the link "Delete Parent"
-# Then a 'DELETE' request is sent to '/parents/:id',
-# the parent is deleted, and all child records are deleted
-# and I am redirected to the parent index page where I no longer see this parent
-
 require 'rails_helper'
 
 RSpec.describe 'destroying a dealership' do
   it 'can delete a dealership' do
     harrison = Dealership.create!(name:"Harrison Ford", is_open: true, max_car_capacity: 352, is_full: true)
 
-    visit '/dealerships'
+    visit "/dealerships/#{harrison.id}"
     click_button "Delete #{harrison.name}"
 
     expect(current_path).to eq('/dealerships')
@@ -27,7 +17,7 @@ RSpec.describe 'destroying a dealership' do
     harrison.employees.create!(name:"Don Keigh", cars_sold:62, on_vacation: false)
     harrison.employees.create!(name:"Sarha Nader", cars_sold:86, on_vacation: true)
 
-    visit '/dealerships'
+    visit "/dealerships/#{harrison.id}"
     click_button "Delete #{harrison.name}"
     visit '/employees'
 
