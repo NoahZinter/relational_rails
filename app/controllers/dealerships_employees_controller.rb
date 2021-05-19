deeb79afffc3ad618a9635cb6cdebab39780fa49
Zach T <ztrokey@gmail.com>
2021-05-19 15:27:24 -0600

    Clean up long route

--- app/controllers/dealerships_employees_controller.rb
+++ app/controllers/dealerships_employees_controller.rb
@@ -15,8 +15,9 @@ class DealershipsEmployeesController < ApplicationController
   end
 
   def create
+    @dealership = Dealership.find(params[:id])
     employee = Dealership.find(params[:id]).employees.create(employee_params)
-    redirect_to "/dealerships/#{Dealership.find(params[:id]).id}/employees"
+    redirect_to "/dealerships/#{@dealership.id}/employees"
   end
 
   private
